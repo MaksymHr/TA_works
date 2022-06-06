2e5018b3e62d30ee49a8c708d7e5a3ea14c7553e
MaksymHr <maksim.hryschenko@gmail.com>
2022-06-06 17:46:42 +0300

    add task

--- TA.cpp
+++ TA.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <string>
 
-bool its_palindrome(std::string line) {
 
-    if (line.length() == 1 || line.length() == 0)
-        return true;
+std::string to_oct(int number, std::string res = "") {
+    
+    if (number < 8)
+        return std::to_string(number);
 
-    if (line[0] == line[line.length() - 1]) {
-        line.erase(line.begin()); line.erase(line.end());
-        its_palindrome(line);
-    }
-    else
-        return false;
+    res = to_oct(number / 8) + std::to_string(number % 8);
 
+    return res;
 }
 
+
+int max_digit_in_number(std::string number, int max = 0) {
+
+    for (int i = 0; i < number.length(); i++) {
+        char a[2] = { number[i] };
+        if (atoi(a) > max) max = atoi(a);
+    }
+    return max;
+}
+
+
 int main() {
+    int n;
 
-    std::string line;
+    std::cout << "Input number: ";
+    std::cin >> n;
 
-    std::cout << "Input string: ";
-    std::cin >> line;
+    std::string n_oct = to_oct(n);
 
-    if (its_palindrome(line))
-        std::cout << "YES\n";
-    else
-        std::cout << "NO\n";
+    std::cout << "Number in oct: " << n_oct << std::endl;
+    std::cout << "Max digit: " << max_digit_in_number(n_oct);
+    
 
     return 0;
 }
